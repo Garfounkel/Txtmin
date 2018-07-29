5cc63008543d6dd6ef8c39fe0557696f52fdf3c1
Thibaud Chominot <thibaud.chominot@epita.fr>
2018-07-29 22:27:21 +0200

    [StringStorage] Fix cut compilation

--- src/string_storage.hxx
+++ src/string_storage.hxx
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <utility>
+
 template <typename CharType>
 typename StringStorage<CharType>::branch_t
 StringStorage<CharType>::EdgeStorage::cut(index_t at) {
   char_t ch = content_[at];
-  edge_t ed = content_.substr(at + 1);
-  content_.resize(at);
-  std::swap(content_, ed);
-  return std::make_pair(ch, ed);
+
+  auto first_half = content_;
+  first_half.resize(at);
+  auto second_half = content_.substr(at + 1);
+
+  edge_t edge(std::move(first_half));
+  content_ = std::move(second_half);
+  return std::make_pair(ch, edge);
 }
