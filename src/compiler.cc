19b5eae5599fe8fc51222829f2270f38c7ad87a0
Simon Andersen <anders_s@epita.fr>
2018-07-30 20:29:28 +0200

    [MISC] Updated compiler main to handle dot option

--- src/compiler.cc
+++ src/compiler.cc
@@ -6,28 +6,12 @@
 
 using storage_t = StringStorage<char>;
 
-void print_results(std::ostream &out,
-                   const PatriciaTrie<storage_t>::results_t &results) {
-  out << "[";
-  for (auto res = std::begin(results); res < std::end(results); res++) {
-    out << "{"
-        << "\"word\":\"" << res->word
-        << "\","
-           "\"freq\":"
-        << res->freq
-        << ","
-           "\"distance\":"
-        << res->distance << "}";
-    if (res != std::end(results) - 1)
-      out << ",";
-  }
-  out << "]" << std::endl;
-}
-
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
+  if (argc != 3 and (argc != 5 or argv[3] != std::string("--dot_output"))) {
     std::cerr << "Usage:" << std::endl;
-    std::cerr << "\t./TextMiningCompiler words_file_path dot_file" << std::endl;
+    std::cerr
+        << "\t./TextMiningCompiler words.txt dict.bin [--dot_ouput dot_file]"
+        << std::endl;
     exit(64);
   }
 
@@ -36,9 +20,11 @@ int main(int argc, char *argv[]) {
 
   auto ptrie = PatriciaTrie<storage_t>::read_words_file(istream);
   std::cerr << "Node number: " << ptrie.node_number_get() << std::endl;
-  auto results = ptrie.search_dist("bar", 10);
-  print_results(std::cout, results);
-  ptrie.write_dot(ostream);
+
+  if (argc == 5 and (argv[3] == std::string("--dot_output"))) {
+    auto dot_ostream = std::ofstream(argv[4]);
+    ptrie.write_dot(dot_ostream);
+  }
 
   return 0;
 }
